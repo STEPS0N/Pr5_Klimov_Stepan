b3c6170f950db11c8891e65e314047250544f692
Степан Климов <stepan.chik228@mail.ru>
2025-09-25 19:19:14 +0500

    Разработанный класс RepoStudents. Климов

--- Pr5/Classes/Student.cs
+++ Pr5/Classes/Student.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace Pr5.Classes
 {
-    internal class Student
+    public class Student
     {
+        public string Firstname = "";
+        public string Lastname = "";
+        public string Surname = "";
+        public bool Scholarship = false;
+        public int Course = 4;
+
+        public Student(string Firstname, string Lastname, string Surname)
+        {
+            this.Firstname = Firstname;
+            this.Lastname = Lastname;
+            this.Surname = Surname;
+        }
+
+        public Student(string Firstname, string Lastname, string Surname, bool Scholarship) : this(Firstname, Lastname, Surname)
+        {
+            this.Scholarship = Scholarship;
+        }
+
+        public Student(string Firstname, string Lastname, string Surname, bool Scholarship, int Course) : this(Firstname, Lastname, Surname, Scholarship)
+        {
+            this.Course = Course;
+        }
+
+        public string GetFIO()
+        {
+            return $"{Lastname} {Firstname} {Surname}";
+        }
     }
 }
